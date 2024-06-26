e89235dd285cc49ebe9386ec06eed961e129d418
Azis_7 <95363625+Azis-7@users.noreply.github.com>
2024-06-26 09:15:39 +0700

    main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,37 @@
 # Simple calculator ruby
-x = eval("2 * 3")
-puts "#{x}"
+print "Input first num! : "
+x = gets.chomp.to_i
+puts "Valid operator is: \n
++ for addition operation
+- for subtraction operation
+* for the multiplication operation
+/ for the division operation
+** for exponent operation
+% for modulus
+"
+print "Input oprator! : "
+y = gets.chomp
+print "Input second num! : "
+z = gets.chomp.to_i
+def calc(x, op, y)
+  case op
+  when '+'
+    x + y
+  when '-'
+    x - y
+  when '*'
+    x * y
+  when '/'
+    x / y.to_f
+  when '**'
+    x ** y
+  when '%'
+    x % y
+  else
+    puts "No valid input"
+  end
+end
+res = calc(x, y, z)
+puts "#{x} #{y} #{z} is #{res}"
+
+
